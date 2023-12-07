e9417e409f7036338989b4198eb3e5592bc3a582
delmac <delalitengue.dev@gmail.com>
2023-12-06 19:05:14 -0800

    done with the main pages

---
+++ app/profile/page.jsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+import ProfileCard from '@components/ProfileCard';
+
+const MyProfile = () => {
+    const router = useRouter();
+    const {data: session} = useSession({
+        required: true,
+        onUnauthenticated(){
+            router.push('/');
+        }
+    });
+    const [posts, setPosts] = useState([]);
+
+    useEffect(()=> {
+        const fetchPosts = async () => {
+          const response = await fetch(`/api/users/${session?.user.id}/posts`);
+    
+          if(response.ok){
+            const data = await response.json();
+    
+            setPosts(data);
+          }
+        };
+    
+        fetchPosts();
+    }, []);
+
+    const handleEdit = () => {
+
+    }
+
+    const handleDelete = async () => {
+
+    }
+
+    return (
+        <ProfileCard
+            name="My"
+            desc="Welcome to ypur personalised profile page"
+            data={posts}
+            handleEdit={handleEdit}
+            handleDelete={handleDelete}
+        />
+    )
+    }
+
+export default MyProfile
